33da6b16a9bbff2e01da83d72147b5b19411a2a2
default2 <default2@default.(none)>
2014-03-05 21:30:38 +0400

    set prefs from script.js cause defaults.js gets overriden

--- data/FF-profile/extensions/tlsnotary@tlsnotary/chrome/content/script.js
+++ data/FF-profile/extensions/tlsnotary@tlsnotary/chrome/content/script.js
@@ -7,6 +7,9 @@ var port;
 port = Components.classes["@mozilla.org/process/environment;1"].getService(Components.interfaces.nsIEnvironment).get("FF_to_backend_port");
 //setting homepage should be done from here rather than defaults.js in order to have the desired effect. FF's quirk.
 Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch("browser.startup.").setCharPref("homepage", "chrome://tlsnotary/content/auditee.html");
+//the 2 prefs below must be set from here rather than defaults.js because TBB overrides them on startup
+Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch("network.proxy.").setIntPref("type", 0);
+Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch("network.proxy.").setBoolPref("socks_remote_dns", false);
 
 //poll the env var to see if IRC started
 //so that we can display a help message on the addon toolbar
